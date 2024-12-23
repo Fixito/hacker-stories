000c72067d01d8a8058c96b2161bb16676076357
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-12-23 13:44:51 +0100

    Refactor App component structure and update ESLint configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,3 @@
-//* Composant React
-// - PascalCase
-// - Doit retourner un élément JSX
-
-export function App1() {
-  // On peut faire du JavaScript ici
-
-  return (
-    <div>
-      <h1>Hello React!</h1>
-    </div>
-  );
-}
-
-// On définit la variable en dehors de la fonction pour éviter de la redéfinir à chaque rendu
-const title = 'React';
-
-export function App2() {
-  return (
-    <div>
-      {/* Affiche une variable dans le JSX */}
-      <h1>Hello {title}!</h1>
-
-      {/* TODO: Faire un <label /> et un <input /> pour un champs de recherche */}
-      {/* Certains attr sont différents en JSX car les noms sont réservés au JS */}
-      <label htmlFor='search'>Search: </label>
-      <input type='search' name='search' id='search' />
-    </div>
-  );
-}
-
-const welcome = {
-  gretting: 'Hey',
-  title: 'React',
-};
-
-// TODO: Afficher le message de bienvenue dans le composant <App />
-
-export function App3() {
-  return (
-    <div>
-      {/* Tout ce qui se trouve entre les accolades est interpolé en JS */}
-      <h1>
-        {welcome.gretting} {welcome.title}!
-      </h1>
-
-      <label htmlFor='search'>Search: </label>
-      <input type='search' name='search' id='search' />
-    </div>
-  );
-}
-
-// Lorsque l'on travaille avec des données en JavaScript, celles-ci se présentent le plus souvent sous la forme d'un tableau d'objets.
-
-// En React, on utilise la méthode map() pour transformer les éléments d'un tableau en JSX en retournant du JSX pour chaque élément.
-
 const list = [
   {
     id: 1,
@@ -73,36 +17,127 @@ const list = [
   },
 ];
 
-// TODO: Afficher le titre des articles dans le composant <App />
-
-export default function App() {
+export function App1() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
+      {/* TODO: Extraire le label et l'input dans un composant <Search :> */}
       <label htmlFor='search'>Search: </label>
       <input type='search' name='search' id='search' />
 
       <hr />
 
-      <ul>
-        {list.map((item) => {
-          const { id, title, author, num_comments, points, url } = item;
-
-          //! On a un warning dans la console du dev tools
-          // Chaque élément doit avoir une prop key unique
-          return (
-            <li key={id}>
-              <a href={url} target='_blank'>
-                {title}
-              </a>
-              <p>{author}</p>
-              <span>Comments: {num_comments}</span>{' '}
-              <span>Points: {points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      {/* On peut diviser une app React en plusieurs composants que l'on peut réutiliser */}
+      <List1 />
     </div>
+  );
+}
+
+function List1() {
+  return (
+    <ul>
+      {list.map((item) => {
+        const { id, title, author, num_comments, points, url } = item;
+
+        return (
+          <li key={id}>
+            <a href={url} target='_blank'>
+              {title}
+            </a>
+            <p>{author}</p>
+            <span>Comments: {num_comments}</span> <span>Points: {points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default function App() {
+  const stories = [
+    {
+      id: 1,
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+    },
+    {
+      id: 2,
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+    },
+  ];
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search />
+
+      <hr />
+
+      {/* Pour passe notre tableau dans le composant <List /> via une prop React */}
+      <List list={stories} />
+    </div>
+  );
+}
+
+function Search() {
+  //* Gérer les fonctions en JSX
+  const handleChange = (e) => {
+    // événement synthétique
+    console.log(e);
+    // valeur de la cible de l'événement (ici l'input HTML)
+    console.log(e.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      {/*! Il faut passer la référence de la fonction et nom l'appeler */}
+      <input type='search' name='search' id='search' onChange={handleChange} />
+    </div>
+  );
+}
+
+function List(props) {
+  return (
+    <ul>
+      {props.list.map((item) => {
+        // const { id, url, title, author, num_comments, points } = item;
+
+        // TODO: Extraire les données dans un comoposant <Item />
+        // return (
+        //   <li key={id}>
+        //     <a href={url} target='_blank'>
+        //       {title}
+        //     </a>
+        //     <p>{author}</p>
+        //     <span>Comments: {num_comments}</span> <span>Points: {points}</span>
+        //   </li>
+        // );
+        return <Item key={item.id} item={item} />;
+      })}
+    </ul>
+  );
+}
+
+function Item(props) {
+  const { id, title, author, num_comments, points, url } = props.item;
+
+  return (
+    <li key={id}>
+      <a href={url} target='_blank'>
+        {title}
+      </a>
+      <p>{author}</p>
+      <span>Comments: {num_comments}</span> <span>Points: {points}</span>
+    </li>
   );
 }
