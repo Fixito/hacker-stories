3ea1d32e3561b96db2adb774a712e0878d95d052
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-12-23 14:38:13 +0100

    Implement search functionality and refactor App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 
-export default function App() {
-  console.log('Rendu de <App />');
-
+// On souhaite récupérer la valeur de l'input Search pour filtrer la liste des stories
+export function App1() {
   const stories = [
     {
       id: 1,
@@ -21,86 +20,103 @@ export default function App() {
       points: 5,
     },
   ];
-
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-
-      <Search />
-
-      <hr />
-
-      {/* Une prop React est utilisée pour passer des données de parent à enfant. */}
-      <List list={stories} />
-    </div>
-  );
-}
-
-export function Search1() {
-  let searchTerm = '';
-
-  const handleChange = (e) => {
-    searchTerm = e.target.value;
-  };
-
-  return (
-    <div>
-      <label htmlFor='search'>Search: </label>
-      <input type='search' name='search' id='search' onChange={handleChange} />
-
-      {/* Si l'on teste ceci dans le navigateur on ne verra pas la sortie apparaître */}
-      {/* Il faut dire à React que searchTerm est un état (state) */}
-      <p>Searching for {searchTerm}</p>
-    </div>
-  );
-}
-
-function Search() {
-  console.log('Rendu de <Search />');
-
-  // Un état React est une structure de données mutable qui est utilisée pour stocker des données qui vont changer au cours du temps.
-  // Lorsqu'un state change, cela déclenche un nouveau rendu (re-render) du composant.
-
-  // l'argument de useState est la valeur initiale de l'état
-  // useState retourne un tableau avec deux éléments :
-  // le premier élément est la valeur actuelle de l'état
-  // le deuxième élément est une fonction qui permet de mettre à jour l'état
+  // On va remonter l'état de l'input Search dans le composant parent <App />
   const [searchTerm, setSearchTerm] = useState('');
 
-  const handleChange = (e) => {
-    // console.log(e);
-    // console.log(e.target.value);
-
+  const handleSearch = (e) => {
     setSearchTerm(e.target.value);
   };
 
   return (
     <div>
-      <label htmlFor='search'>Search: </label>
-      <input type='search' name='search' id='search' onChange={handleChange} />
+      <h1>My Hacker Stories</h1>
 
-      <p>Searching for {searchTerm}</p>
+      <Search searchTerm={searchTerm} onSearch={handleSearch} />
+
+      <hr />
+
+      {/* TODO: Essayer de filtrer la liste avec le state `searchTerm` en utlisant la méthode filter() */}
+      <List list={stories} />
     </div>
   );
 }
 
-function List(props) {
-  console.log('Rendu de <List />');
+export default function App() {
+  const stories = [
+    {
+      id: 1,
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+    },
+    {
+      id: 2,
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+    },
+  ];
+  // Si on initialise la valeur, on constate que l'input de l'affiche pas
+  // Il faut que la valeur de l'input soit contrôlée par le state `searchTerm`
+  const [searchTerm, setSearchTerm] = useState('react');
+
+  const searchedStories = stories.filter((s) =>
+    s.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search search={searchTerm} onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={searchedStories} />
+    </div>
+  );
+}
+
+function Search({ search, onSearch }) {
+  const handleChange = (e) => {
+    onSearch(e);
+  };
+
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input
+        type='search'
+        name='search'
+        id='search'
+        value={search}
+        onChange={handleChange}
+      />
+
+      <p>Searching for {search}</p>
+    </div>
+  );
+}
+
+function List({ list }) {
+  return (
     <ul>
-      {props.list.map((item) => {
+      {list.map((item) => {
         return <Item key={item.id} item={item} />;
       })}
     </ul>
   );
 }
 
-function Item(props) {
-  console.log('Rendu de <Item />');
-
-  const { id, title, author, num_comments, points, url } = props.item;
-
+function Item({ id, title, author, num_comments, points, url }) {
   return (
     <li key={id}>
       <a href={url} target='_blank'>
