263b0e0a0c3a3c2880a4a5106a370c0aeb543eac
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-12-24 11:33:24 +0100

    Add axios for API calls and refactor App component for improved search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,53 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import axios from 'axios';
+import { useCallback, useEffect, useId, useState } from 'react';
 
-// Ce serait bien que notre app se souvienne de notre dernière recherche (pour l'UX)
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
-export function App1() {
-  const stories = [
-    {
-      id: 1,
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-    },
-    {
-      id: 2,
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-    },
-  ];
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
 
-  const [searchTerm, setSearchTerm] = useState(
-    localStorage.getItem('search') || 'React'
-  );
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [key, value]);
 
-  const searchedStories = stories.filter((s) =>
-    s.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  // La fonctionnalité fonctionne mais on a un souci. Il y a un side-effect dans notre handler. Si on utilise setSearchTerm ailleurs, on ne pourra pas garantir que le localStorage sera mis à jour. On peut utiliser un useEffect pour ça.
-  const handleSearch = (e) => {
-    setSearchTerm(e.target.value);
-    localStorage.setItem('search', e.target.value);
-  };
-
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-
-      <Search search={searchTerm} onSearch={handleSearch} />
-
-      <hr />
-
-      <List list={searchedStories} />
-    </div>
-  );
-}
+  return [value, setValue];
+};
 
 export default function App() {
-  const stories = [
-    {
-      id: 1,
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-    },
-    {
-      id: 2,
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-    },
-  ];
-
-  const [searchTerm, setSearchTerm] = useState(
-    localStorage.getItem('search') ?? 'React'
-  );
+  const [stories, setStories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
   const searchedStories = stories.filter((s) =>
-    s.title.toLowerCase().includes(searchTerm.toLowerCase())
+    s.title?.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  const fetchStories = useCallback(async () => {
+    setIsLoading(true);
+
+    try {
+      const { data } = await axios(API_ENDPOINT + searchTerm);
+      setStories(data.hits);
+      setIsError(false);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [searchTerm]);
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
+  };
+
+  const handleRemoveStory = (id) => {
+    setStories(stories.filter((s) => s.id !== id));
   };
 
   useEffect(() => {
-    localStorage.setItem('search', searchTerm);
-  }, [searchTerm]);
+    fetchStories();
+  }, [fetchStories]);
 
   return (
     <div>
@@ -92,9 +55,15 @@ export default function App() {
 
       <Search search={searchTerm} onSearch={handleSearch} />
 
+      {isError && <p>Something went wrong ...</p>}
+
       <hr />
 
-      <List list={searchedStories} />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 }
@@ -106,38 +75,78 @@ function Search({ search, onSearch }) {
 
   return (
     <div>
-      <label htmlFor='search'>Search: </label>
-      <input
-        type='search'
+      <InputWithLabel
+        label='Search:'
         name='search'
-        id='search'
+        type='search'
+        isFocused
         value={search}
-        onChange={handleChange}
-      />
+        onInputChange={handleChange}
+      >
+        <strong>Search: </strong>
+      </InputWithLabel>
 
       <p>Searching for {search}</p>
     </div>
   );
 }
 
-function List({ list }) {
+function InputWithLabel({
+  children,
+  isFocused,
+  name,
+  onInputChange,
+  type = 'text',
+  value,
+}) {
+  const id = useId();
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      <input
+        type={type}
+        name={name}
+        id={id}
+        value={value}
+        onChange={onInputChange}
+        autoFocus={isFocused}
+      />
+    </>
+  );
+}
+
+function List({ list, onRemoveItem }) {
   return (
     <ul>
       {list.map((item) => {
-        return <Item key={item.id} {...item} />;
+        return (
+          <Item key={item.objectID} {...item} onRemoveItem={onRemoveItem} />
+        );
       })}
     </ul>
   );
 }
 
-function Item({ id, title, author, num_comments, points, url }) {
+function Item({
+  objectID,
+  title,
+  author,
+  num_comments,
+  points,
+  url,
+  onRemoveItem,
+}) {
   return (
-    <li key={id}>
+    <li key={objectID}>
       <a href={url} target='_blank'>
         {title}
       </a>
       <p>{author}</p>
       <span>Comments: {num_comments}</span> <span>Points: {points}</span>
+      <div>
+        <button onClick={() => onRemoveItem(objectID)}>Delete</button>
+      </div>
     </li>
   );
 }
