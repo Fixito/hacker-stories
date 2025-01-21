5e2761c64eb0e63e6c7178ef6ce11558a0b498e1
Thomas ROBERT <thomas_3004@hotmail.fr>
2025-01-21 12:57:50 +0100

    Add LastSearches component and enhance search functionality; update App component to manage multiple search URLs and display last searches

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useCallback, useEffect, useReducer, useState } from 'react';
 
 import axios from 'axios';
 
-import SearchForm from './components/SearchForm.jsx';
 import List from './components/List.jsx';
+import SearchForm from './components/SearchForm.jsx';
 
 import { useStorageState } from './hooks/useStorageState.js';
 
@@ -15,19 +15,52 @@ import {
   STORIES_FETCH_SUCCESS,
   storiesReducer,
 } from './storiesReducer.js';
+import LastSearches from './components/LastSearches.jsx';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
+function extractSearchTerm(url) {
+  return url.replace(API_ENDPOINT, '');
+}
+
+function getLastSearches(urls) {
+  return urls
+    .reduce((result, url, index) => {
+      const searchTerm = extractSearchTerm(url);
+
+      if (index === 0) {
+        return [...result, searchTerm];
+      }
+
+      const previousSearchTerm = result.at(-1);
+
+      if (searchTerm === previousSearchTerm) {
+        return result;
+      } else {
+        return [...result, searchTerm];
+      }
+    }, [])
+
+    .slice(-6)
+    .slice(0, -1)
+    .map((url) => extractSearchTerm(url));
+}
+
+function getUrl(searchTerm) {
+  return `${API_ENDPOINT}${searchTerm}`;
+}
 
 export default function App() {
   const [stories, dispatchStories] = useReducer(storiesReducer, initialState);
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
-  const [url, setUrl] = useState(`${API_ENDPOINT}${searchTerm}`);
+  const [urls, setUrls] = useState([getUrl(searchTerm)]);
 
   const handleFetchStories = useCallback(async () => {
     dispatchStories({ type: STORIES_FETCH_INIT });
 
     try {
-      const { data } = await axios(url);
+      const lastUrl = urls.at(-1);
+      const { data } = await axios(lastUrl);
       dispatchStories({
         type: STORIES_FETCH_SUCCESS,
         payload: data.hits,
@@ -35,16 +68,28 @@ export default function App() {
     } catch {
       dispatchStories({ type: STORIES_FETCH_FAILURE });
     }
-  }, [url]);
+  }, [urls]);
 
   const handleSearchInput = (e) => {
     setSearchTerm(e.target.value);
   };
 
+  const handleSearch = (searchTerm) => {
+    const url = getUrl(searchTerm);
+    setUrls([...urls, url]);
+  };
+
+  const handleLastSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    handleSearch(searchTerm);
+  };
+
+  const lastSearches = getLastSearches(urls);
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     if (!searchTerm) return;
-    setUrl(`${API_ENDPOINT}${searchTerm}`);
+    handleSearch(searchTerm);
   };
 
   const handleRemoveStory = useCallback((id) => {
@@ -63,6 +108,11 @@ export default function App() {
         searchTerm={searchTerm}
         onInputChange={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
+      />
+
+      <LastSearches
+        lastSearches={lastSearches}
+        onLastSearch={handleLastSearch}
       />
 
       {stories.isError && <p>Something went wrong ...</p>}
---
+++ src/components/LastSearches.jsx
@@ -0,0 +1,15 @@
+export default function LastSearches({ lastSearches, onLastSearch }) {
+  return (
+    <>
+      {lastSearches.map((searchTerm, index) => (
+        <button
+          key={searchTerm + index}
+          type='button'
+          onClick={() => onLastSearch(searchTerm)}
+        >
+          {searchTerm}
+        </button>
+      ))}
+    </>
+  );
+}
